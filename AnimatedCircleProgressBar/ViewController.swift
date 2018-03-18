634771030cb7701d14e25ba4eee5caa43afa145f
Eduardo Carrillo <edcarril@ucsd.edu>
2018-03-17 21:32:44 -0700

    Drew circle with core animation.

--- AnimatedCircleProgressBar/ViewController.swift
+++ AnimatedCircleProgressBar/ViewController.swift
@@ -12,7 +12,20 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let shapeLayer = CAShapeLayer()
+        
+        let center = view.center
+        let radius = 100
+        let startAngle = 0
+        let endAngle = 2*CGFloat.pi
+        let isClockwise = true
+        
+        let circularPath  = UIBezierPath(arcCenter: center, radius: CGFloat(radius), startAngle: CGFloat(startAngle), endAngle: endAngle, clockwise: isClockwise)
+        
+        shapeLayer.path = circularPath.cgPath
+        
+        view.layer.addSublayer(shapeLayer)
     }
 
     override func didReceiveMemoryWarning() {
