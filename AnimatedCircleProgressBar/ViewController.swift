a5d82b79ca6b4ad40b77c82ddeefc41526b74a60
Eduardo Carrillo <edcarril@ucsd.edu>
2018-03-17 22:17:25 -0700

    Added track and smoother animation.

--- AnimatedCircleProgressBar/ViewController.swift
+++ AnimatedCircleProgressBar/ViewController.swift
@@ -21,13 +21,31 @@ class ViewController: UIViewController {
         
         let center = view.center
         let radius = 100
-        let startAngle = 0
-        let endAngle = 2*CGFloat.pi
+        let startAngle = -1*CGFloat.pi/2
+        let endAngle = 3*CGFloat.pi/2
         let isClockwise = true
+        
+        
         
         let circularPath  = UIBezierPath(arcCenter: center, radius: CGFloat(radius), startAngle: CGFloat(startAngle), endAngle: endAngle, clockwise: isClockwise)
         
+        
+        let trackLayer = CAShapeLayer()
+        
+        trackLayer.path = circularPath.cgPath
+        //Rounder line
+        trackLayer.lineCap = kCALineCapRound
+        trackLayer.fillColor = UIColor.clear.cgColor
+        
+        trackLayer.strokeColor = UIColor.gray.cgColor
+        trackLayer.lineWidth = 10
+        
+        view.layer.addSublayer(trackLayer)
+        
         shapeLayer.path = circularPath.cgPath
+        //Rounder line
+        shapeLayer.lineCap = kCALineCapRound
+        shapeLayer.fillColor = UIColor.clear.cgColor
         
         shapeLayer.strokeColor = UIColor.red.cgColor
         shapeLayer.lineWidth = 10
@@ -49,6 +67,10 @@ class ViewController: UIViewController {
         //When user taps we should animate the stroke
         let basicAnimation = CABasicAnimation(keyPath: strokeEndProperty)
         basicAnimation.toValue = 1.0
+        
+        basicAnimation.duration = 2
+       basicAnimation.isRemovedOnCompletion = false
+        basicAnimation.fillMode = kCAFillModeForwards
         
         shapeLayer.add(basicAnimation, forKey: basicAnimationKey)
         
